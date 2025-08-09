363f70f271a31b66657445e36c2a8a07543cae9a
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-09 14:05:36 +0300

    Adds class Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # store lowercase for easier comparison
+
+    def match(self, candidates):
+        matches = []
+        for candidate in candidates:
+            if sorted(candidate.lower()) == sorted(self.word) and candidate.lower() != self.word:
+                matches.append(candidate)
+        return matches
+
+
